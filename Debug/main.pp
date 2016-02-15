34d8e746f986ea0f3923ea2e98294a177798be77
cddedric <ilkspyro@yahoo.com>
2016-02-15 10:56:45 -0500

    First commit

---
+++ Debug/main.pp
@@ -0,0 +1,101 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/msp432.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/msp432p401r.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/system_msp432p401r.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/driverlib.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/adc14.h
+main.obj: C:/ti/ccsv6/ccs_base/arm/include/msp.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/aes256.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/comp_e.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/cpu.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/crc32.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/cs.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/dma.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/eusci.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/flash.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/fpu.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/gpio.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/i2c.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/interrupt.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/mpu.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/pcm.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/pmap.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/pss.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/ref_a.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/reset.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/rom.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/rom_map.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/rtc_c.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/spi.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/sysctl.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/systick.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/timer32.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/timer_a.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/uart.h
+main.obj: C:/ECE461/ECE461_Challenge1/driverlib/wdt_a.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/string.h
+
+../main.c: 
+C:/ti/ccsv6/ccs_base/arm/include/msp432.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+C:/ti/ccsv6/ccs_base/arm/include/system_msp432p401r.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/driverlib.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/adc14.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/aes256.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/comp_e.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/cpu.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/crc32.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/cs.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/dma.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/eusci.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/flash.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/fpu.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/gpio.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/i2c.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/interrupt.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/mpu.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/pcm.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/pmap.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/pss.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/ref_a.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/reset.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/rom.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/rom_map.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/rtc_c.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/spi.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/sysctl.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/systick.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/timer32.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/timer_a.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/uart.h: 
+C:/ECE461/ECE461_Challenge1/driverlib/wdt_a.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/string.h: 
---
+++ main.c
@@ -0,0 +1,69 @@
+ //*****************************************************************************
+//
+// MSP432 main.c template - Empty main
+//
+//****************************************************************************
+
+#include "msp432.h"
+#include "driverlib.h"
+
+#include "string.h"
+
+#define BUFFER_MEMORY_START 0x000200000
+
+char memoryOffset = 0;
+
+void port_init()			//	port initialization function
+{
+
+	P1DIR |= BIT0;			// make P1.0 an output (RED LED)
+	P1DIR &= ~BIT4;			// make P1.4 and input
+	P1REN |= BIT4;			// enable pull resistor on P1.4
+	P1OUT |= BIT4;			// make it a pull-up resistor
+	P2DIR |= BIT1;			// make P2.1 an output (GREEN LED)
+
+	P1OUT &= ~BIT0;			//turn off RED LED
+	P2OUT &= ~BIT1;			//turn off GREEN LED
+
+}
+void delay_1sec()      	// 1 second delay for 1.5 MHz clock
+{
+	int  dcntr;			// delay counter variable
+
+	for (dcntr=0;dcntr<57692;dcntr++);		//~1 second delay loop
+	return;
+}
+
+void main(void)
+{
+	port_init();
+	memoryOffset = 0;					//making sure my offset is set to 0
+	WDTCTL = WDTPW |WDTSSEL_3|WDTIS_4|WDTCNTCL;	// Watchdog timer set to expire every second
+
+	while(1){
+		WDTCTL = WDTPW | WDTCNTCL;		//clear watchdog each loop of the almighty while
+	if (!(P1IN & BIT4)){				//check if button is pushed and initiate functions if true
+		P1OUT |= BIT0;					//turn on RED LED
+		delay_1sec();					//tie up the processor for roughly one second
+		WDTCTL = (WDTPW-1);				//force a reset by the watchdog not getting the correct password when passed information or attempting changes
+	} //outside of button push
+
+	uint8_t buffer[4096];
+	memset(buffer,0xA5,4096);
+	WDTCTL = WDTPW | WDTCNTCL;
+	MAP_FlashCtl_unprotectSector(FLASH_INFO_MEMORY_SPACE_BANK0,FLASH_SECTOR0<<memoryOffset);
+	WDTCTL = WDTPW | WDTCNTCL;
+	while(!MAP_FlashCtl_eraseSector(BUFFER_MEMORY_START));
+    WDTCTL = WDTPW | WDTCNTCL;
+    while(!MAP_FlashCtl_programMemory(buffer,(void*) BUFFER_MEMORY_START, 4096 ));
+    WDTCTL = WDTPW | WDTCNTCL;
+    MAP_FlashCtl_protectSector(FLASH_INFO_MEMORY_SPACE_BANK0,FLASH_SECTOR0<<memoryOffset);
+    WDTCTL = WDTPW | WDTCNTCL;
+    memoryOffset++;
+    if (memoryOffset>=32){
+    	P2OUT |= BIT1;
+    	delay_1sec();
+    	WDTCTL = (WDTPW-1);
+    }
+	} //end of almighty while
+} // end of main
